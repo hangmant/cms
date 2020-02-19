101f50ee333931a62a29a852f1b4e53e0b088677
Dante Calderon <mail@dantecalderon.dev>
2020-02-19 02:18:22 -0500

    Create words table

---
+++ pages/words.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import { get } from 'lodash'
+import { IconButton } from '@material-ui/core'
+import DelteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
+import NextLink from 'next/link'
+import AddIcon from '@material-ui/icons/Add'
+import Fab from '@material-ui/core/Fab'
+
+const columns = [
+  { id: 'name', key: 'name', label: 'Name', minWidth: 170 },
+  { id: 'category', key: 'category.name', label: 'Category', minWidth: 100 },
+  { id: 'options', key: 'name', label: 'Options', minWidth: 100 },
+]
+
+const rows = [
+  {
+    _id: '1',
+    name: 'PERU',
+    category: {
+      _id: '1',
+      name: 'Geografia',
+    },
+  },
+  {
+    _id: '2',
+    name: 'Ceviche',
+    category: {
+      _id: '2',
+      name: 'Comida',
+    },
+  },
+  {
+    _id: '3',
+    name: 'Maria',
+    category: {
+      _id: '45',
+      name: 'Personas',
+    },
+  },
+]
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  container: {
+    maxHeight: 440,
+  },
+}))
+
+export default function Words() {
+  const classes = useStyles()
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+
+  const handleDeleteWord = id => () => {}
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                  <TableCell key={`name${index}`}>{get(row, 'name')}</TableCell>
+                  <TableCell key={`category-${index}`}>{get(row, 'category.name')}</TableCell>
+                  <TableCell key={`options${index}`}>
+                    <div>
+                      <NextLink href={`/words/edit/${row._id}`}>
+                        <IconButton>
+                          <EditIcon />
+                        </IconButton>
+                      </NextLink>
+                      <IconButton onClick={handleDeleteWord(row._id)}>
+                        <DelteIcon />
+                      </IconButton>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+
+      <Fab aria-label={'fab.label'} className={classes.fab} color={'primary'}>
+        <NextLink href={`/words/new`}>
+          <AddIcon />
+        </NextLink>
+      </Fab>
+    </Paper>
+  )
+}
--- src/Layout.js
+++ src/Layout.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
 import useLocalStorage from '../hooks/useLocalStorage'
-import { MENU_ITEMS } from '../constants/menu';
-import NextLink from 'next/link';
+import { MENU_ITEMS } from '../constants/menu'
+import NextLink from 'next/link'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -77,20 +77,20 @@ const useStyles = makeStyles(theme => ({
     }),
     marginLeft: 0,
   },
-}));
+}))
 
 export default function PersistentDrawerLeft({ children }) {
-  const classes = useStyles();
-  const theme = useTheme();
+  const classes = useStyles()
+  const theme = useTheme()
   const [open, setOpen] = useState(true)
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
     <div className={classes.root}>
@@ -112,7 +112,7 @@ export default function PersistentDrawerLeft({ children }) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Persistent drawer
+            Hangman CMS
           </Typography>
         </Toolbar>
       </AppBar>
@@ -150,8 +150,8 @@ export default function PersistentDrawerLeft({ children }) {
         })}
       >
         <div className={classes.drawerHeader} />
-       {children}
+        {children}
       </main>
     </div>
-  );
+  )
 }
