b8fff5300461efcad37e72c25c85f5d470078d30
Dante Calderon <mail@dantecalderon.dev>
2020-02-23 03:23:47 -0500

    Remove apollo-boost, add apollo client manually

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { onError } from 'apollo-link-error'
+import { HttpLink } from 'apollo-link-http'
+import withApollo from 'next-with-apollo'
 import App from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from '@material-ui/core/styles'
-import withApollo from 'next-with-apollo'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import theme from '../src/theme'
-import { ApolloProvider } from '@apollo/react-hooks'
-import ApolloClient, { InMemoryCache } from 'apollo-boost'
-
+import React from 'react'
 import Layout from '../src/Layout'
+import theme from '../src/theme'
+
 class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
@@ -40,9 +44,31 @@ class MyApp extends App {
   }
 }
 
-export default withApollo(({ initialState }) => {
-  return new ApolloClient({
-    uri: 'http://localhost:8087/graphql',
-    cache: new InMemoryCache().restore(initialState || {}),
-  })
-})(MyApp)
+export default withApollo(
+  (
+    {
+      /* initialState */
+    }
+  ) => {
+    return new ApolloClient({
+      link: ApolloLink.from([
+        onError(({ graphQLErrors, networkError }) => {
+          if (graphQLErrors) {
+            graphQLErrors.forEach(({ message, locations, path }) => {
+              console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+              )
+            })
+          }
+          if (networkError) {
+            console.log(`[Network error]: ${networkError}`)
+          }
+        }),
+        new HttpLink({
+          uri: 'http://localhost:8087/graphql',
+        }),
+      ]),
+      cache: new InMemoryCache(),
+    })
+  }
+)(MyApp)
