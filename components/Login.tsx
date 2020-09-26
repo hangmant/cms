dc705ac26ca6950c63e4a5f265581e9037d8a0f4
Dante Calderón <mail@dantecalderon.dev>
2020-09-26 11:27:37 -0500

    feat: Improve login (#25)
    * Login on enter
    * Improve errors

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,24 +3,23 @@ import { window } from 'browser-monads'
 import fetch from 'isomorphic-fetch'
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import * as Yup from 'yup'
 import { setCookie } from '../apis/session'
 import { config } from '../config/config'
 import { ResLoginLocal } from '../interfaces/res-login-local.interface'
 import Separator from './shared/Separator'
+import { useFormik } from 'formik'
 
 type LoginProps = {
   handleToggleForm: () => any
 }
 
 export const Login = (props: LoginProps) => {
-  const [username, setUsername] = useState<string>('calderon@gmail.com')
-  const [password, setPassword] = useState<string>('mailero')
-
   const redirectToDashboard = () => {
     window.location.replace('/words')
   }
 
-  const onClickLogIn = async () => {
+  const onClickLogIn = async ({ email, password }) => {
     try {
       const response = await fetch(`${config.hangwomanApiREST}/auth/login/jwt`, {
         method: 'POST',
@@ -28,7 +27,7 @@ export const Login = (props: LoginProps) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username,
+          username: email,
           password,
         }),
       })
@@ -47,28 +46,49 @@ export const Login = (props: LoginProps) => {
     }
   }
 
+  const formik = useFormik({
+    initialValues: {
+      email: 'dante@gmail.com',
+      password: 'dante',
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email('Invalid Email').required('Required!'),
+      password: Yup.string().required('Required!'),
+    }),
+    onSubmit: onClickLogIn,
+  })
+  console.log('Dante: formik', formik.errors)
+
   return (
     <React.Fragment>
-      <TextField
-        variant="outlined"
-        label="Email Address"
-        fullWidth
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <Separator v={12} />
-      <TextField
-        variant="outlined"
-        label="Password"
-        fullWidth
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <Separator v={12} />
-      <Button size="large" fullWidth onClick={onClickLogIn} variant="contained" color="primary">
-        Login
-      </Button>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          variant="outlined"
+          label="Email Address"
+          helperText={formik.errors.email}
+          error={Boolean(formik.errors.email)}
+          name="email"
+          fullWidth
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <Separator v={12} />
+        <TextField
+          variant="outlined"
+          helperText={formik.errors.password}
+          error={Boolean(formik.errors.password)}
+          label="Password"
+          name="password"
+          fullWidth
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <Separator v={12} />
+        <Button size="large" fullWidth type="submit" variant="contained" color="primary">
+          Login
+        </Button>
+      </form>
       <Separator v={12} />
       <Divider />
       <Separator v={6} />
